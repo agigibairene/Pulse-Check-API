41f753350a7f0abbe63196a56de77c722140fcac
agigibairene <i.agigiba@alustudent.com>
2026-06-08 20:05:41 +0000

    implemented views for status, monitor, pause-monitor

--- pulse_check_api/monitor_api/urls.py
+++ pulse_check_api/monitor_api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import CreateMonitorView
+from .views import CreateMonitorView, PauseMonitorView, PulseView, MonitorStatusView
 
 
 urlpatterns = [
-    path('', CreateMonitorView.as_view(), name='register-device')
+    path('', CreateMonitorView.as_view(), name='register-device'),
+    path('<str:id>/heartbeat/', PulseView.as_view()),
+    path('<str:id>/pause/', PauseMonitorView.as_view(), name='pause-monitor'),
+    path('<str:id>/monitor-device/', MonitorStatusView.as_view(), name='monitor-a-device')
 ]
--- pulse_check_api/monitor_api/views.py
+++ pulse_check_api/monitor_api/views.py
@@ -2,6 +2,8 @@ from rest_framework import generics, status
 from .serializers import MonitorSerializer
 from .models import MonitorModel
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
 
 
 class CreateMonitorView(generics.ListCreateAPIView):
@@ -19,4 +21,49 @@ class CreateMonitorView(generics.ListCreateAPIView):
             status=status.HTTP_201_CREATED
         )
         
+
+
+class PulseView(generics.GenericAPIView):
     
+    def post(self, request, id):
+        
+        monitor = get_object_or_404(MonitorModel, id=id)
+        
+        monitor.last_beat = timezone.now()
+        monitor.status = MonitorModel.STATUS.Active
+        monitor.alert_sent = False
+        monitor.save()
+        
+        return Response(
+            {
+                "message": "Heartbeat received"
+            }
+        )
+        
+
+
+class PauseMonitorView(generics.GenericAPIView):
+    """ Allows user to pause the beat/monitor"""
+    def post(self, request, id):
+        monitor = get_object_or_404(MonitorModel, id=id)
+        
+        monitor.status = MonitorModel.STATUS.Paused
+        monitor.save(update_fields=['status'])
+        return Response(
+            {"message": "Monitor paused"}
+        )
+        
+
+class MonitorStatusView(generics.GenericAPIView):
+    """ Monitors the status of a specific device"""
+    def get(self, request, id):
+
+        monitor = get_object_or_404(MonitorModel,id=id)
+
+        return Response({
+            "id": monitor.id,
+            "status": monitor.status,
+            "timeout": monitor.timeout,
+            "last_beat": monitor.last_beat,
+            "alert_email": monitor.alert_email,
+        })
